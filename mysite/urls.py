adf23c25439b30968c8e01f8f0db1a64a041816c
LavishGulati <lavish00gulati@gmail.com>
2019-05-30 15:35:33 +0530

    Copied static files

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('club/', include('CodingClubIITG.urls')),
+    path('home/', include('CodingClubIITG.urls')),
     path('admin/', admin.site.urls),
 ]
 
